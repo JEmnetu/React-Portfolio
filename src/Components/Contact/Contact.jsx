c3c50a16bf927b14b2a943a692ad4733d8a43418
Jacob Habtemariam <jacob@Jacobs-MBP.home>
2020-03-17 01:53:20 -0400

    added contact component styling

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import './Contact.css';
 
 const Contact = () => {
     return ( 
-    <div id="target">
-    <div className="jumbotron">Contact Me
+    // <div id="target">
+    // <div className="jumbotron" id="main-text">Contact Me
     
-    <p>Phone, Email</p></div>
-    </div> );
+    // <p>Phone, Email</p></div>
+    // </div> );
+    <div className="container-fluid" id="target">
+            
+           <div className="row">
+               <div className="jumbotron mx-auto" id="main-text">
+                   <h1>Let's Connect!</h1>
+                   <span>Github</span> <span>LinkedIn</span>
+               </div>
+           </div>
+        
+
+        </div>)
 }
  
 export default Contact;
