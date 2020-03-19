dea58156dc7d5100bfb0e9e44aa3b6a9071b567a
Jacob Habtemariam <jacob@Jacobs-MBP.home>
2020-03-19 01:29:38 -0400

    updated contact  div id name

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -14,8 +14,9 @@ const Contact = (props) => {
                <div className="jumbotron mx-auto" id="main-text">
                    <h1>Let's Connect!</h1>
     
-                   <div className="row mt-5" id="contactDiv">
-                   <a href={props.github} target="_blank" rel="noopener noreferrer"> <i class="fab fa-github fa-4x"></i></a>  <a href={props.linkedin} target="_blank" rel="noopener noreferrer" ><i class="fab fa-linkedin fa-4x"></i></a>
+                   <div className="row mt-5" id="connectDiv">
+                   <a href={props.github} target="_blank" rel="noopener noreferrer"> <i class="fab fa-github fa-4x"></i></a> 
+                   <a href={props.linkedin} target="_blank" rel="noopener noreferrer" ><i class="fab fa-linkedin fa-4x"></i></a>
                    </div>
                    
                </div>
