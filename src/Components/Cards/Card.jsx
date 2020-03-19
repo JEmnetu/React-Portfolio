1d114eaf79e13559dfdad6850ccab4ed71ba50d6
Jacob Habtemariam <jacob@Jacobs-MBP.home>
2020-03-19 01:50:58 -0400

    corrected deploy button links

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Card.css';
 
 class Card extends Component {
     state = {  }
@@ -8,7 +9,7 @@ class Card extends Component {
           <img src={this.props.image} className="card-img-top" alt="Project"></img>
             <div className="card-body">
             <h5 className="card-title">{this.props.name}</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            
             <div className="row">
             <a href={this.props.github} target="_blank" rel="noopener noreferrer" className="btn btn-danger mb-1" >Github Repo </a>
             <a href={this.props.deploy} target="_blank" rel="noopener noreferrer" className="btn btn-danger" >Deployed Application </a>
