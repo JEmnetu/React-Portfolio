b8a3490db7cb4ecce876a12596357382323ef2a5
Jacob Habtemariam <jacob@Jacobs-MBP.home>
2020-03-20 01:17:38 -0400

    updated navbar to react-bootstrap

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,37 +1,54 @@
 import React, { Component } from 'react';
-import './Navbar.css';
+import{Navbar, Button, FormControl, Form, Nav } from 'react-bootstrap';
+import './Navigation.css';
 
 
-class Navbar extends Component {
+class Navigation extends Component {
     
     render() { 
-        return ( <div className="navbar navbar-expand-lg">
-        <a className="navbar-brand" href="/">My Portfolio</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      //   return ( <div className="navbar navbar-expand-lg">
+      //   <a className="navbar-brand" href="/">My Portfolio</a>
+      //   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      //     <span className="navbar-toggler-icon"></span>
+      //   </button>
       
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
+      //   <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      //     <ul className="navbar-nav mr-auto">
 
-            <li className="nav-item">
-              <a className="nav-link" href="/">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/Projects">Projects</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/Contact">Contact</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href={this.props.resume} target="_blank" rel="noopener noreferrer">Resume</a>
-            </li>
+      //       <li className="nav-item">
+      //         <a className="nav-link" href="/">Home</a>
+      //       </li>
+      //       <li className="nav-item">
+      //         <a className="nav-link" href="/Projects">Projects</a>
+      //       </li>
+      //       <li className="nav-item">
+      //         <a className="nav-link" href="/Contact">Contact</a>
+      //       </li>
+      //       <li className="nav-item">
+      //         <a className="nav-link" href={this.props.resume} target="_blank" rel="noopener noreferrer">Resume</a>
+      //       </li>
 
-          </ul>
+      //     </ul>
           
-        </div>
-      </div> );
+      //   </div>
+      // </div> );
+      return(
+<div>
+      <Navbar  style={{ backgroundColor:'rgb(100, 0, 0)',
+    color:"white",
+    borderBottom: "black solid 6px"}}>
+      <Navbar.Brand href="/">My Portfolio</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/Projects">Projects</Nav.Link>
+        <Nav.Link href="/Contact">Contact</Nav.Link>
+        <Nav.Link href={this.props.resume}>Resume</Nav.Link>
+      </Nav>
+    
+    </Navbar>
+   
+    </div>);
     }
 }
  
-export default Navbar;
+export default Navigation;
