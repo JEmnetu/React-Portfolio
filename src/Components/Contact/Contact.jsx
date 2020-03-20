0b7bf3a9d1caf1218fa94695c2b529ecb5901c63
Jacob Habtemariam <jacob@Jacobs-MBP.home>
2020-03-20 01:46:49 -0400

    restyled contact page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -11,12 +11,17 @@ const Contact = (props) => {
     <div className="container-fluid" id="target">
             
            <div className="row">
-               <div className="jumbotron mx-auto" id="main-text">
+
+           {/* <div className="col-xs-1 offset-2"> <a href={props.github} target="_blank" rel="noopener noreferrer"> <i class="fab fa-github fa-4x"></i></a></div>
+                   <div className="col-xs-1"><a href={props.linkedin} target="_blank" rel="noopener noreferrer" ><i class="fab fa-linkedin fa-4x"></i></a></div> */}
+               <div className="col-xs-1 jumbotron mx-auto" id="main-text">
                    <h1 id="mainHeader">Let's Connect!</h1>
-                   <h3>{props.email}</h3>
+                   <ul>
+                <li><a href={props.github} rel="noopener noreferrer">Github </a></li>
+                <li><a href={props.linkedin} rel="noopener noreferrer">Linkedin</a></li>
+                   </ul>
                    <div className="row mt-4" id="connectDiv">
-                   <a href={props.github} target="_blank" rel="noopener noreferrer"> <i class="fab fa-github fa-4x"></i></a> 
-                   <a href={props.linkedin} target="_blank" rel="noopener noreferrer" ><i class="fab fa-linkedin fa-4x"></i></a>
+                   
                    </div>
                    
                </div>
