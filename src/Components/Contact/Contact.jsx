145ec6f760056c1c63cb2074076bd4b3948ed1f4
Jacob Habtemariam <jacob@Jacobs-MBP.home>
2020-03-20 21:18:37 -0400

    made the contact page more responsive. Gave social icons a hover effect

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -14,15 +14,17 @@ const Contact = (props) => {
 
            {/* <div className="col-xs-1 offset-2"> <a href={props.github} target="_blank" rel="noopener noreferrer"> <i class="fab fa-github fa-4x"></i></a></div>
                    <div className="col-xs-1"><a href={props.linkedin} target="_blank" rel="noopener noreferrer" ><i class="fab fa-linkedin fa-4x"></i></a></div> */}
-               <div className="col-xs-1 jumbotron mx-auto" id="main-text">
+               <div className="col-xs-5 jumbotron mx-auto" id="main-text">
+
                    <h1 id="mainHeader">Let's Connect!</h1>
                    <ul>
-                <li><a href={props.github} rel="noopener noreferrer">Github </a></li>
-                <li><a href={props.linkedin} rel="noopener noreferrer">Linkedin</a></li>
+                        <li><a href={props.github} rel="noopener noreferrer"><i class="fab fa-github fa-3x"></i></a></li>
+                        <li><a href={props.linkedin} rel="noopener noreferrer"><i class="fab fa-linkedin fa-3x"></i></a></li>
+                       
                    </ul>
-                   <div className="row mt-4" id="connectDiv">
+                   {/* <div className="row mt-4" id="connectDiv">
                    
-                   </div>
+                   </div> */}
                    
                </div>
            </div>
